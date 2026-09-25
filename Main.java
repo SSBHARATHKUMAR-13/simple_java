983028ceaab46960c1ba4c13027543de63fdc13d
BHARATHKUMAR S S <24z127@psgitech.ac.in>
2026-09-25 09:13:43 +0530

    Add greeting message to Main.java

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins!");
         System.out.println("Java project is working successfully by bharath");
+        System.out.println("Hi");
     }
 }
